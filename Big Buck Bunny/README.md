afc467dae3e38fe8bf3ad388164de570bf36a8ff
fervi <lordfervi@protonmail.com>
2019-10-23 16:27:23 +0200

    Added a script for "Big Buck Bunny".

---
+++ Big Buck Bunny/README.md
@@ -0,0 +1,17 @@
+# Big Buck Bunny
+
+To run the script correctly, download this file and save it as input.mp4
+
+```
+http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_native_60fps_normal.mp4
+```
+
+Or use the command below.
+
+```
+wget http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_native_60fps_normal.mp4 -O input.mp4
+```
+
+___
+
+Now run the script and wait for the completion of the task (it can take up to a few days).
---
+++ Big Buck Bunny/main.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Thumbnail
+ffmpeg -i input.mp4 -vf scale=700:400 -ss 00:00:03.000 -vframes 1 thumb.png
+
+# Low Quality
+ffmpeg -y -i input.mp4 -c:v libvpx -r 30 -b:v 0 -crf 20 -pass 1 -an -f webm /dev/null
+ffmpeg -y -i input.mp4 -c:v libvpx -r 30 -b:v 0 -crf 20 -vf scale=320:240 -pass 2 -c:a libvorbis 240p.webm
+ffmpeg -y -i input.mp4 -c:v libvpx -r 30 -b:v 0 -crf 20 -vf scale=480:360 -pass 2 -c:a libvorbis 360p.webm
+ffmpeg -y -i input.mp4 -c:v libvpx -r 30 -b:v 0 -crf 20 -vf scale=640:480 -pass 2 -c:a libvorbis 480p.webm
+ffmpeg -y -i input.mp4 -c:v libvpx -r 30 -b:v 0 -crf 20 -vf scale=720:576 -pass 2 -c:a libvorbis 576p.webm
+rm ffmpeg2pass-0.log
+
+# High Quality
+ffmpeg -y -i input.mp4 -c:v libvpx -b:v 0 -crf 15 -pass 1 -an -f webm /dev/null
+ffmpeg -y -i input.mp4 -c:v libvpx -b:v 0 -crf 15 -vf scale=1280:720 -pass 2 -c:a libvorbis 720p.webm
+ffmpeg -y -i input.mp4 -c:v libvpx -b:v 0 -crf 15 -vf scale=1920:1080 -pass 2 -c:a libvorbis 1080p.webm
+ffmpeg -y -i input.mp4 -c:v libvpx -b:v 0 -crf 15 -vf scale=3840:2160 -pass 2 -c:a libvorbis 2160p.webm
+rm ffmpeg2pass-0.log
