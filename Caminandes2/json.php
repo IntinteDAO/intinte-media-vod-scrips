f54238f4fb261de6c57afea0675303c1193ec501
fervi <lordfervi@protonmail.com>
2019-10-31 15:18:37 +0100

    Added JSON generator for Caminandes 2

---
+++ Caminandes2/json.php
@@ -0,0 +1,20 @@
+<?php
+
+$movie["title"]="Caminandes: Gran Dillama";
+$movie["author"]="Blender Foundation";
+$movie["description"]="The films centers on the llama Koro in Patagonia and his attempts to overcome various obstacles. Koro hunts for food on the other side of a fence and is once again inspired by the Armadillo but this time to a shocking effect.";
+$movie["hidden"]=0;
+$movie["license"]="CC BY 3.0";
+$movie["website"]="http://www.caminandes.com";
+$movie["rating"]=5;
+$movie["genre"][0]=3;
+$movie["genre"][1]=9;
+$movie["genre"][2]=19;
+$movie["subtitles"]="";
+$movie["enchanted"]=0;
+$movie["next_episode"]="";
+$movie["previous_episode"]="";
+
+echo json_encode($movie);
+
+?>
