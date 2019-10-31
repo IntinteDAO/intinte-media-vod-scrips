eff0a42b6f2ca7c0bcd6ee227a1ee46e96d0944d
fervi <lordfervi@protonmail.com>
2019-10-31 15:42:30 +0100

    Added JSON generator for Big Buck Bunny

---
+++ Big Buck Bunny/json.php
@@ -0,0 +1,20 @@
+<?php
+
+$movie["title"]="Big Buck Bunny";
+$movie["author"]="Blender Foundation";
+$movie["description"]="The plot follows a day in the life of Big Buck Bunny, during which time he meets three bullying rodents: Frank the flying squirrel (the leader), and Rinky and Gimera (his sidekicks). The rodents amuse themselves by harassing helpless creatures of the forest by throwing fruits, nuts, and rocks at them.";
+$movie["hidden"]=0;
+$movie["license"]="CC BY 3.0";
+$movie["website"]="https://peach.blender.org";
+$movie["rating"]=4;
+$movie["genre"][0]=3;
+$movie["genre"][1]=9;
+$movie["genre"][2]=19;
+$movie["subtitles"]="";
+$movie["enchanted"]=0;
+$movie["next_episode"]="";
+$movie["previous_episode"]="";
+
+echo json_encode($movie);
+
+?>
