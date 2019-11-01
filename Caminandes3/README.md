aa231c80c8fda9a11c6b43ed0e1cc751dd74e3dc
fervi <lordfervi@protonmail.com>
2019-11-01 17:43:57 +0100

    Added a script for "Caminandes 3: Llamigos".

---
+++ Caminandes3/README.md
@@ -0,0 +1,17 @@
+# Caminandes: Llamigos
+
+To run the script correctly, download this file.
+
+```
+http://www.caminandes.com/download/03_caminandes_llamigos_1080p.mp4
+```
+
+Or use the command below.
+
+```
+wget http://www.caminandes.com/download/03_caminandes_llamigos_1080p.mp4
+```
+
+___
+
+Now run the script and wait for the completion of the task (it can take up to a few hours).
---
+++ Caminandes3/json.php
@@ -0,0 +1,20 @@
+<?php
+
+$movie["title"]="Caminandes: Llamigos";
+$movie["author"]="Blender Foundation";
+$movie["description"]="The films centers on the llama Koro in Patagonia and his attempts to overcome various obstacles. Koro meets Oti, a pesky magellanic penguin, in an epic battle over tasty red berries during the winter.";
+$movie["hidden"]=0;
+$movie["license"]="CC BY 4.0";
+$movie["website"]="http://www.caminandes.com";
+$movie["rating"]=5;
+$movie["genre"][0]=3;
+$movie["genre"][1]=9;
+$movie["genre"][2]=19;
+$movie["subtitles"]="";
+$movie["enchanted"]=0;
+$movie["next_episode"]="";
+$movie["previous_episode"]="";
+
+echo json_encode($movie);
+
+?>
