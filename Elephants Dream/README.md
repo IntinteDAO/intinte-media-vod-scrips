3a3a08af3f01872a611bc43e1aecaea21726a3fd
fervi <lordfervi@protonmail.com>
2019-11-06 18:42:43 +0100

    Added scripts for Elephants Dream

---
+++ Elephants Dream/README.md
@@ -0,0 +1,17 @@
+# Elephants Dream
+
+To run the script correctly, download this file and save it as input.avi.
+
+```
+https://archive.org/download/ElephantsDream/ed_hd.avi
+```
+
+Or use the command below.
+
+```
+wget https://archive.org/download/ElephantsDream/ed_hd.avi
+```
+
+___
+
+Now run the script and wait for the completion of the task (it can take up to a few days).
---
+++ Elephants Dream/json.php
@@ -0,0 +1,18 @@
+<?php
+
+$movie["title"]="Elephants Dream";
+$movie["author"]="Blender Foundation";
+$movie["description"]="Elephants Dream is the story of two strange characters exploring a capricious and seemingly infinite machine. The elder, Proog, acts as a tour-guide and protector, happily showing off the sights and dangers of the machine to his initially curious but increasingly skeptical protege Emo. As their journey unfolds we discover signs that the machine is not all Proog thinks it is, and his guiding takes on a more desperate aspect.";
+$movie["hidden"]=0;
+$movie["license"]="CC BY 2.5";
+$movie["website"]="https://orange.blender.org";
+$movie["rating"]=4;
+$movie["genre"][0]=3;
+$movie["genre"][1]=19;
+$movie["enchanted"]=0;
+$movie["next_episode"]="";
+$movie["previous_episode"]="";
+
+echo json_encode($movie);
+
+?>
