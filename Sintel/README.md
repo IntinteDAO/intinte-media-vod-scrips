fbbe4d7de7857641467b3e6b9d0a2b6625901ebb
fervi <lordfervi@protonmail.com>
2019-11-08 15:28:06 +0100

    Added scripts for Sintel

---
+++ Sintel/README.md
@@ -0,0 +1,17 @@
+# Sintel
+
+To run the script correctly, download this file and save it as input.mkv.
+
+```
+https://download.blender.org/durian/movies/Sintel.2010.4k.mkv
+```
+
+Or use the command below.
+
+```
+wget https://download.blender.org/durian/movies/Sintel.2010.4k.mkv -O input.mkv
+```
+
+___
+
+Now run the script and wait for the completion of the task (it can take up to a few days).
---
+++ Sintel/json.php
@@ -0,0 +1,19 @@
+<?php
+
+$movie["title"]="Sintel";
+$movie["author"]="Blender Foundation";
+$movie["description"]="The main character, Sintel, is attacked while traveling through a wintry mountainside. After defeating her attacker and taking his spear, she finds refuge in a shaman's hut. He asks her why she is traveling, and she confesses that she is looking for a dragon, leading into a flashback. Sintel was a homeless loner, looking for food when she discovered an injured baby dragon. She nursed him back to health and named him Scales, and the two quickly formed an emotional bond. One day, while Scales was flying, he was captured by an adult dragon. Determined to get him back, she began the long and dangerous journey that led her to the shaman's hut.";
+$movie["hidden"]=0;
+$movie["license"]="CC BY 3.0";
+$movie["website"]="https://durian.blender.org";
+$movie["rating"]=5;
+$movie["genre"][0]=3;
+$movie["genre"][1]=10;
+$movie["genre"][2]=19;
+$movie["enchanted"]=0;
+$movie["next_episode"]="";
+$movie["previous_episode"]="";
+
+echo json_encode($movie);
+
+?>
