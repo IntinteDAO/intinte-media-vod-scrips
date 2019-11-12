a3e70fdaedf5eb11210e2e8568e94361d6a07f88
fervi <lordfervi@protonmail.com>
2019-11-12 18:14:49 +0100

    The movie "Tears of Steel" has been added.

---
+++ Tears of Steel/README.md
@@ -0,0 +1,17 @@
+# Tears of Steel
+
+To run the script correctly, download this file and save it as input.mkv.
+
+```
+https://download.blender.org/demo/movies/ToS/tearsofsteel_4k.mov
+```
+
+Or use the command below.
+
+```
+wget https://download.blender.org/demo/movies/ToS/tearsofsteel_4k.mov -O input.mov
+```
+
+___
+
+Now run the script and wait for the completion of the task (it can take up to a few days).
---
+++ Tears of Steel/json.php
@@ -0,0 +1,18 @@
+<?php
+
+$movie["title"]="Tears of Steel";
+$movie["author"]="Blender Foundation";
+$movie["description"]='The short begins with a view of a rocket lifting off. The camera than pans to a bridge, where Thom (Vanja Rukavina) and Celia (Denise Rebergen) are standing on, arguing. Thom then says to her that both of them need to follow their passions (Thom wants to be "awesome in space", and Celia with her robotics). Celia then convinces him to tell her that he is freaked out by her robotic hand. When Thom admits it, the two break up.';
+$movie["hidden"]=0;
+$movie["license"]="CC BY 3.0";
+$movie["website"]="https://mango.blender.org";
+$movie["rating"]=5;
+$movie["genre"][0]=18;
+$movie["genre"][1]=19;
+$movie["enchanted"]=0;
+$movie["next_episode"]="";
+$movie["previous_episode"]="";
+
+echo json_encode($movie);
+
+?>
